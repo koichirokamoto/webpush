3e9776c8f7030eeb341fcc684467774f42def950
Koichiro Okamoto <koichiro.okamoto@topgate.co.jp>
2016-06-07 20:02:25 +0900

    update

--- encrypto.go
+++ encrypto.go
@@ -4,22 +4,32 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"math/big"
 	"math/rand"
 	"strings"
 )
 
-func createServerKey(clientKey string) (ecdsa.PublicKey, error) {
-	p, err := ecdsa.GenerateKey(elliptic.P256(), strings.NewReader(clientKey))
+// Encrypt is encryption info.
+type Encrypt struct {
+	Payload    []byte
+	Salt       []byte
+	PublickKey []byte
+}
+
+func createServerKey() ([]byte, *big.Int, *big.Int, error) {
+	salt, _ := createSalt(32)
+	p, x, y, err := elliptic.GenerateKey(elliptic.P256(), bytes.NewReader(salt))
 	if err != nil {
-		return ecdsa.PublicKey{}, err
+		return nil, nil, nil, err
 	}
-	return p.PublicKey, nil
+	return p, x, y, nil
 }
 
 func hkdf(salt, ikm, info []byte, l int) ([]byte, error) {
@@ -28,36 +38,27 @@ func hkdf(salt, ikm, info []byte, l int) ([]byte, error) {
 	}
 
 	h := hmac.New(sha256.New, salt)
-	hu := h.Sum(ikm)
+	h.Write(ikm)
+	hu := h.Sum(nil)
 
 	i := hmac.New(sha256.New, hu)
-	iu := i.Sum([]byte{1})
-	return iu[0 : l], nil
+	i.Write(info)
+	i.Write([]byte{1})
+	return i.Sum(nil)[0:l], nil
 }
 
-func createInfo(ty, cl, sv string) []byte {
-	l := len([]byte(ty))
-	i := make([]byte, 18+l+1+5+1+2+65+2+65)
-	w := bytes.NewBuffer(i)
-	w.WriteString("'Content-Encoding: '")
+func createInfo(ty string, ctx []byte) []byte {
+	w := bytes.NewBuffer([]byte{})
+	w.WriteString("Content-Encoding: ")
 	w.WriteString(ty)
-	w.Write(nil)
-	w.WriteString("'P-256'")
-	w.Write(nil)
-	bc := make([]byte, 2)
-	binary.BigEndian.PutUint16(bc, uint16(len(cl)))
-	w.Write(bc)
-	w.WriteString(cl)
-	bs := make([]byte, 2)
-	binary.BigEndian.PutUint16(bs, uint16(len(sv)))
-	w.Write(bs)
-	w.WriteString(sv)
+	w.WriteByte(0)
+	w.WriteString("P-256")
+	w.Write(ctx)
 	return w.Bytes()
 }
 
-func createSalt() ([]byte, error) {
-	c := 16
-	b := make([]byte, c)
+func createSalt(i int) ([]byte, error) {
+	b := make([]byte, i)
 	_, err := rand.Read(b)
 	if err != nil {
 		return nil, err
@@ -65,41 +66,66 @@ func createSalt() ([]byte, error) {
 	return b, nil
 }
 
+func createContext(cl, sv []byte) []byte {
+	buf := bytes.NewBuffer([]byte{})
+	buf.WriteByte(0)
+	bc := make([]byte, 2)
+	binary.BigEndian.PutUint16(bc, uint16(len(cl)))
+	buf.Write(bc)
+	buf.Write(cl)
+	bs := make([]byte, 2)
+	binary.BigEndian.PutUint16(bs, uint16(len(sv)))
+	buf.Write(bs)
+	buf.Write(sv)
+	return buf.Bytes()
+}
+
 // GetEncryptoMessage returns encrypted messsage for web push api.
-func GetEncryptoMessage(cl, auth, sv string, raw []byte) ([]byte, error) {
-	svkey, err := createServerKey(cl)
-	if err != nil {
-		return nil, err
-	}
-	b := bytes.NewBuffer([]byte("'Content-Encoding: auth"))
-	b.Write(nil)
-	b.WriteString("'")
-	ikm := elliptic.Marshal(svkey.Curve, svkey.X, svkey.Y)
-	prk, err := hkdf([]byte(auth), ikm, b.Bytes(), 32)
-	if err != nil {
-		return nil, err
-	}
-	info := createInfo("aesgcm", cl, sv)
-	salt, err := createSalt()
-	if err != nil {
-		return nil, err
-	}
-	key, err := hkdf(salt, prk, info, 16)
+func GetEncryptoMessage(cl, auth string, raw []byte) (*Encrypt, error) {
+	dc, err := base64.URLEncoding.DecodeString(cl)
 	if err != nil {
 		return nil, err
 	}
 
-	ni := createInfo("nonce", cl, sv)
+	da, err := base64.URLEncoding.DecodeString(auth)
+	if err != nil {
+		return nil, err
+	}
+
+	salt, _ := createSalt(16)
+
+	priv, x, y, err := createServerKey()
+	if err != nil {
+		return nil, err
+	}
+	pub := elliptic.Marshal(elliptic.P256(), x, y)
+
+	px, py := elliptic.Unmarshal(elliptic.P256(), dc)
+	ix, _ := elliptic.P256().ScalarMult(px, py, priv)
+
+	b := bytes.NewBufferString("Content-Encoding: auth")
+	b.WriteByte(0)
+
+	prk, err := hkdf(da, ix.Bytes(), b.Bytes(), 32)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := createContext(dc, pub)
+
+	ci := createInfo("aesgcm", ctx)
+	ck, err := hkdf(salt, prk, ci, 16)
+	if err != nil {
+		return nil, err
+	}
+
+	ni := createInfo("nonce", ctx)
 	n, err := hkdf(salt, prk, ni, 12)
 	if err != nil {
 		return nil, err
 	}
 
-	pad := make([]byte, 2)
-	buf := bytes.NewBuffer(pad)
-	buf.WriteByte(0)
-	buf.WriteByte(0)
-	bl, err := aes.NewCipher(key)
+	bl, err := aes.NewCipher(ck)
 	if err != nil {
 		return nil, err
 	}
@@ -109,7 +135,38 @@ func GetEncryptoMessage(cl, auth, sv string, raw []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	var result []byte
-	_ = cp.Seal(result, n, raw, buf.Bytes())
-	return result, nil
+	buf := makePadding(0)
+	buf.Write(raw)
+	result := cp.Seal([]byte{}, n, buf.Bytes(), nil)
+	printAll(priv, pub, salt)
+	printAll(ix.Bytes(), prk, ci, ck, ni, n, result)
+	return &Encrypt{
+		Payload:    result,
+		Salt:       salt,
+		PublickKey: pub,
+	}, nil
+}
+
+func convertBase64(b []byte) string {
+	s := base64.URLEncoding.EncodeToString(b)
+	s = strings.Replace(s, "+", "-", -1)
+	s = strings.Replace(s, "/", "_", -1)
+	s = strings.Replace(s, "=", "", -1)
+	return s
+}
+
+func makePadding(l int) *bytes.Buffer {
+	be := make([]byte, 2)
+	binary.BigEndian.PutUint16(be, uint16(l))
+	buf := bytes.NewBuffer(be)
+	for i := 0; i < l; i++ {
+		buf.WriteByte(0)
+	}
+	return buf
+}
+
+func printAll(b ...[]byte) {
+	for _, v := range b {
+		fmt.Println(convertBase64(v))
+	}
 }
--- encrypto_test.go
+++ encrypto_test.go
@@ -2,30 +2,16 @@ package webpushencrypto
 
 import "testing"
 
-func TestCreateSalt(t *testing.T) {
-	_, err := createSalt()
+func TestGetEncryptoMessage(t *testing.T) {
+	b64k := "BClJhggKh-J5TiGi05P926XK5r7XdKOGHQTxnMx53_raRUpgrFBcUPVCWdBOrW8ofJjZfpG2bjgYsSacJiIjee4="
+	b64a := "KvVpa60hivfIjBEq1I76zA=="
+	res, err := GetEncryptoMessage(b64k, b64a, []byte("Hello, world!"))
 	if err != nil {
 		t.Fatal(err)
 	}
-}
 
-func TestCreateInfo(t *testing.T) {
-	_ = createInfo("aesgcm", "clinetpublickey", "serverpublickey")
-}
-
-func TestHkdf(t *testing.T) {
-	salt, _ := createSalt()
-	ikm := []byte{1, 2, 3, 4, 5, 6}
-	info := []byte{7, 8, 9}
-	_, err := hkdf(salt, ikm, info, 32)
-	if err != nil {
-		t.Fatal(err)
+	if res == nil {
+		t.Fatal("result is empty")
 	}
-}
-
-func TestCreateServerKey(t *testing.T) {
-	_, err := createServerKey("NSeFRZehVM:APA91bEDKm9dC-1T2B7TKExAVGDAXRJqgCQncrxCUc4SfbXUrClZxk8HmOuB3eC17UTb8oge6Fnuq86CrCpSLRjKg56kOFgqhiFVwNp3_lHQOi2-zOgn6GWM_1vD-5OrPh9QAQAd2KnX")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Log(res.Payload)
 }
